5a34b78b50ec54fc535710ce9a1bdefddf2b3673
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-12-26 17:48:04 -0300

    refactor: better preview logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -81,132 +81,103 @@ export default function Home() {
     return undefined;
   }, []);
 
-  const preview = useCallback(
-    (nodes: INode[], editingNode: INode): { nodes: INode[]; changeId: string } => {
-      if (!editingNode.id) return { nodes, changeId: '' };
-      if (nodes[0].id === '0' && nodes[0].children?.length === 0)
-        return { nodes: [{ ...nodes[0], children: [editingNode] }], changeId: '0' };
-      console.log('editing node', editingNode);
-      const nodesPreview: INode[] = [];
-      let changeId = '';
-      nodes.forEach(node => {
-        console.log('current node', node.label, node);
-        if (editingNode.id === node.id) {
-          // current node is the editing node
-          console.log('current node is the editing node');
-          if (editingNode.order === node.order) {
-            // current node order is equal to editing node order
-            console.log('current node order is equal to editing node order');
-            // add editing node with that order
-            nodesPreview.push(editingNode);
-          } else if (editingNode.order < node.order) {
-            // current node order is greater than editing node order
-            console.log('current node order is different to editing node order');
-            // set current node as deleting
-            nodesPreview.push({ ...node, modified: EnumModified.DELETING, children: [] });
-          }
-          changeId = editingNode.id;
-          return;
-        }
-        if (editingNode.parent !== node.parent) {
-          // current node has different parent as editing node
-          console.log('current node has different parent as editing node');
-          if (node.children?.length > 0) {
-            // current node has children
-            console.log('current node has children');
-            // call the function recursively to get the preview of the children
-            const { nodes: childrenPreview, changeId: childrenChangeId } = preview(
-              [...node.children],
-              editingNode,
-            );
-            let { modified } = node;
-            if (
-              childrenPreview.find(
-                child => child.id === childrenChangeId || child.id === editingNode.id,
-              )
-            ) {
-              // the children preview has the editing node
-              console.log('the children preview has the editing node');
-              // set current node as updating
-              changeId = childrenChangeId;
-              modified = childrenChangeId ? EnumModified.UPDATING : modified;
-            }
-
-            nodesPreview.push({
-              ...node,
-              children: childrenPreview,
-              modified,
-            });
-          } else {
-            nodesPreview.push({ ...node });
-          }
-          return;
-        }
-        // current node has same parent as editing node
-        console.log('current node has same parent as editing node');
-        if (node.order < editingNode.order) {
-          // current node order is less than editing node order
-          console.log('current node order is less than editing node order');
-          // add current node to preview
-          nodesPreview.push({ ...node });
-        } else if (node.order === editingNode.order) {
-          // current node order is equal to editing node order
-          console.log('current node order is equal to editing node order');
-          // deleting operation was already handled
-          // add editing node with current node order
-          nodesPreview.push(editingNode);
-          // add current node to preview with order + 1
-          nodesPreview.push({
-            ...node,
-            order: node.order + 1,
-            modified: EnumModified.UPDATING,
-          });
-          changeId = node.id;
+  const preview = useCallback((nodes: INode[], editingNode: INode): INode[] => {
+    if (!editingNode.id) return nodes;
+    if (nodes[0].id === '0' && nodes[0].children?.length === 0)
+      return [{ ...nodes[0], children: [editingNode] }];
+    console.log('nodes', nodes);
+    console.log('editing node', editingNode);
+    const nodesPreview: INode[] = [];
+    nodes.forEach(node => {
+      console.log('current node', node);
+      if (node.id === editingNode.id) {
+        // editing node is current node
+        console.log('editing node is current node', node);
+        if (editingNode.modified === EnumModified.UPDATING) {
+          // editing node is updating
+          console.log('editing node is updating');
+          // set current node as updating
+          nodesPreview.push({ ...node, modified: EnumModified.UPDATING });
         } else {
-          // current node order is greater than editing node order
-          console.log('current node order is greater than editing node order');
-          // if deleting, decrease the order of the current node
-          let operation = -1;
-          if (
-            editingNode.modified === EnumModified.INSERTING ||
-            editingNode.modified === EnumModified.UPDATING
-          ) {
-            // creating or updating
-            console.log('creating or updating');
-            // increase the order of the current node
-            operation = 1;
-          }
+          // editing node is deleting
+          console.log('editing node is deleting');
+          // set current node as deleting
+          nodesPreview.push({ ...node, modified: EnumModified.DELETING });
+        }
+      } else if (node.id === editingNode.parent) {
+        // current node is parent of editing node
+        console.log('editing node is parent of current node', node);
+        if (!node.children || node.children.length === 0) {
+          // parent has no children
+          console.log('parent has no children');
+          // add editing node to parent children
           nodesPreview.push({
             ...node,
-            order: node.order + operation,
+            children: [editingNode],
             modified: EnumModified.UPDATING,
           });
-          changeId = node.id;
-        }
-      });
-      if (!changeId) {
-        // there is no change
-        console.log('there is no change');
-        const parent = findNodeById(nodesPreview, editingNode.parent);
-        if (parent) {
-          // editing node has parent
-          console.log('editing node has parent', parent);
-          // add editing node to parent children
-          const children = parent.children || [];
-          if (!children.find(child => child.id === editingNode.id)) {
-            // editing node is not in parent children
-            console.log('editing node is not in parent children', children);
-            parent.children = [...children, editingNode];
-            parent.modified = EnumModified.UPDATING;
-            changeId = editingNode.id;
+        } else {
+          const children = node.children.map(child => {
+            if (child.id === editingNode.id) {
+              // child is editing node
+              console.log('child is editing node', child);
+              if (editingNode.modified === EnumModified.DELETING) {
+                // editing node is deleting
+                console.log('editing node is deleting');
+                // set child as deleting
+                return { ...child, modified: EnumModified.DELETING };
+              }
+              // editing node is updating
+              console.log('editing node is updating');
+              // set child as updating
+              return { ...child, modified: EnumModified.UPDATING };
+            }
+            if (child.order >= editingNode.order) {
+              // child is after editing node
+              console.log('child is after editing node', child);
+              // set child order + 1 and set as updating
+              return { ...child, order: child.order + 1, modified: EnumModified.UPDATING };
+            }
+            // child is before editing node
+            console.log('child is before editing node', child);
+            return child;
+          });
+          if (editingNode.modified === EnumModified.INSERTING) {
+            // editing node is inserting
+            console.log('editing node is inserting');
+            // add editing node to parent children
+            children.splice(editingNode.order - 1, 0, editingNode);
           }
+          // set parent as updating
+          nodesPreview.push({ ...node, children, modified: EnumModified.UPDATING });
+        }
+      } else {
+        // current node is not parent of editing node
+        console.log('current node is not parent of editing node', node);
+        if (node.children?.length > 0) {
+          // current node has children
+          console.log('current node has children', node);
+          // add current node to preview
+          const children = preview(node.children, editingNode);
+          if (JSON.stringify(children) !== JSON.stringify(node.children)) {
+            nodesPreview.push({ ...node, children, modified: EnumModified.UPDATING });
+          } else {
+            // children has no changes
+            console.log('children has no changes');
+            // add current node to preview
+            nodesPreview.push(node);
+          }
+        } else {
+          // current node has no children
+          console.log('current node has no children');
+          // add current node to preview
+          nodesPreview.push(node);
         }
       }
-      console.log('nodes preview', nodesPreview, changeId);
-      return { nodes: nodesPreview, changeId };
-    },
-    [findNodeById],
-  );
+    });
+    console.log('nodes preview', nodesPreview);
+    return nodesPreview;
+  }, []);
 
   const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
     setExpanded(nodeIds);
@@ -247,7 +218,7 @@ export default function Home() {
           }
           setEditingNode({
             ...selectedNode,
-            modified: EnumModified.INSERTING,
+            modified: EnumModified.UPDATING,
           });
           setSelected(selectedNode.id);
           break;
@@ -271,6 +242,7 @@ export default function Home() {
     (nodes: INode[]) =>
       nodes.map(node => {
         let color = 'black';
+        const fontWeight = node.id === editingNode.id ? 'bold' : 'normal';
         switch (node.modified) {
           case EnumModified.INSERTING:
             color = 'green';
@@ -291,6 +263,7 @@ export default function Home() {
               sx={{
                 '& > .MuiTreeItem-content  > .MuiTreeItem-label': {
                   color,
+                  fontWeight,
                 },
               }}
             >
@@ -311,7 +284,7 @@ export default function Home() {
           />
         );
       }),
-    [],
+    [editingNode.id],
   );
 
   const renderOperationScreen = () => {
@@ -599,7 +572,7 @@ export default function Home() {
               onNodeToggle={handleToggle}
               onNodeSelect={handleSelect}
             >
-              {renderNodes(preview(nodes, editingNode).nodes)}
+              {renderNodes(preview(nodes, editingNode))}
             </TreeView>
           </Box>
         </Box>
