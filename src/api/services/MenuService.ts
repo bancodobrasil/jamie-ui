1c85eb2d932911ac6d2c9d26f2cc15d9ebc3bdd6
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-12-26 21:27:06 -0300

    feat: MenuService api getListMenuSync

--- src/api/services/MenuService.ts
+++ src/api/services/MenuService.ts
@@ -36,4 +36,33 @@ export default class MenuService {
     });
     return wrapPromise(promise);
   }
+
+  static async getListMenuSync(payload: GetListMenuPayload): Promise<GetListMenuResponse> {
+    // TODO: Implement the API request
+    // The Promise below simulates the loading time of the request, remove it when you implement the request itself.
+    const promise = new Promise<GetListMenuResponse>((resolve, reject) => {
+      setTimeout(() => {
+        // reject(new Error('Failed to fetch RuleSheet'));
+        resolve({
+          data: [
+            {
+              id: 1,
+              name: 'Menu Mobile',
+            },
+            {
+              id: 2,
+              name: 'Menu AAPF',
+            },
+          ],
+          total: 2,
+          page: 1,
+          limit: 10,
+          hasNextPage: false,
+          hasPreviousPage: false,
+        });
+      }, 2000);
+    });
+    const response = await promise;
+    return response;
+  }
 }
