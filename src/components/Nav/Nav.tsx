97e4f0eaa6ae5e4b6d2305c4bd32666aefc7c339
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-01-02 15:42:14 -0300

    feat: login and logout button

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,13 +6,20 @@ import {
   Box,
   SelectChangeEvent,
   Typography,
+  Button,
 } from '@mui/material';
+import { useKeycloak } from '@react-keycloak/web';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Nav = () => {
+  const { keycloak } = useKeycloak();
+
+  const location = useLocation();
+
   const { t, i18n } = useTranslation();
+
   const [language, setLanguage] = useState<string>(i18n.resolvedLanguage);
 
   const handleLanguageChange = (event: SelectChangeEvent) => {
@@ -22,23 +29,44 @@ export const Nav = () => {
   };
 
   const renderLanguageSwitcher = () => (
-    <Box sx={{ display: 'flex', justifyContent: 'flex-end', flex: 1 }}>
-      <FormControl sx={{ minWidth: 160 }}>
-        <InputLabel id="demo-simple-select-label">{t('language.title')}</InputLabel>
-        <Select
-          variant="standard"
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={language}
-          label={t('language.title')}
-          onChange={handleLanguageChange}
-        >
-          <MenuItem value="en">English</MenuItem>
-          <MenuItem value="pt-BR">Português Brasileiro</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <FormControl sx={{ minWidth: 160 }}>
+      <InputLabel id="demo-simple-select-label">{t('language.title')}</InputLabel>
+      <Select
+        variant="standard"
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={language}
+        label={t('language.title')}
+        onChange={handleLanguageChange}
+      >
+        <MenuItem value="en">English</MenuItem>
+        <MenuItem value="pt-BR">Português Brasileiro</MenuItem>
+      </Select>
+    </FormControl>
   );
+
+  const onLogoutClickHandler = () => {
+    keycloak?.logout();
+  };
+
+  const onLoginClickHandler = () => {
+    keycloak?.login({ redirectUri: `${process.env.JAMIE_UI_BASE_URL}${location.pathname}` });
+  };
+
+  const renderLoginButton = () => {
+    if (keycloak?.authenticated) {
+      return (
+        <Box>
+          <Button onClick={onLogoutClickHandler}>Logout</Button>
+        </Box>
+      );
+    }
+    return (
+      <Box>
+        <Button onClick={onLoginClickHandler}>Login</Button>
+      </Box>
+    );
+  };
 
   return (
     <Box
@@ -58,7 +86,13 @@ export const Nav = () => {
           </Typography>
         </Link>
       </Box>
-      {renderLanguageSwitcher()}
+      <Box
+        sx={{ display: 'flex', justifyContent: 'flex-end', flex: 1, alignItems: 'center' }}
+        className="space-x-4"
+      >
+        {renderLanguageSwitcher()}
+        {renderLoginButton()}
+      </Box>
     </Box>
   );
 };
