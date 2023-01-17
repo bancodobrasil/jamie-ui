1685b91174ef97db47c2b648fdff9e944cf574fc
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-01-17 14:15:16 -0300

    feat: continue loading until redirect to keycloak auth page

--- src/components/App/AppRoutes/ProtectedRoute.tsx
+++ src/components/App/AppRoutes/ProtectedRoute.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 import { useKeycloak } from '@react-keycloak/web';
 import { JAMIE_UI_BASE_URL } from '../../../constants';
+import Loading from '../../Loading';
 
 interface Props {
   children?: JSX.Element;
@@ -13,9 +14,10 @@ export const ProtectedRoute = ({ children }: Props): JSX.Element => {
 
   const location = useLocation();
 
-  if (!keycloak?.authenticated)
+  if (!keycloak?.authenticated) {
     keycloak?.login({ redirectUri: `${JAMIE_UI_BASE_URL}${location.pathname}` });
-
+    return <Loading />;
+  }
   if (children) return children;
 
   return <Outlet />;
