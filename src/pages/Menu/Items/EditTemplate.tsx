b84e510e1a2f0e3ac487425ae1d623db62768611
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-02-07 22:08:49 -0300

    feat: EditTemplate page CodeMirror editor with javascript language

--- src/pages/Menu/Items/EditTemplate.tsx
+++ src/pages/Menu/Items/EditTemplate.tsx
@@ -3,6 +3,8 @@ import { Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@apollo/client';
 import { useNavigate, useParams } from 'react-router-dom';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 import { AppBreadcrumbs } from '../../../components/AppBreadcrumbs';
 import MenuItemService from '../../../api/services/MenuItemService';
 import Loading from '../../../components/Loading';
@@ -15,6 +17,8 @@ export const EditTemplate = () => {
 
   const { t } = useTranslation();
 
+  const [template, setTemplate] = React.useState("console.log('hello world!');");
+
   const { loading, error, data } = useQuery(MenuItemService.GET_MENU_ITEM, {
     variables: { id: Number(itemId) },
   });
@@ -22,6 +26,10 @@ export const EditTemplate = () => {
   const onBackClickHandler = () => {
     navigate('/');
   };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    setTemplate(value);
+  }, []);
 
   if (loading) return <Loading />;
 
@@ -76,6 +84,16 @@ export const EditTemplate = () => {
         >
           {t('menuItem.editTemplate.title')}
         </Typography>
+        <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', flex: 1 }}>
+          <CodeMirror
+            value={template}
+            height="200px"
+            extensions={[javascript({ jsx: true })]}
+            onChange={onChange}
+            minHeight="60vh"
+            minWidth="40vw"
+          />
+        </Box>
       </Box>
     </Box>
   );
