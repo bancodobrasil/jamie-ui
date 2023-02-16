f5f88a18a4f93efd4ace7a695d8d5c873b5d8354
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-02-16 01:34:57 -0300

    feat: better initial templates

--- src/utils/template/MenuInitialTemplate.ts
+++ src/utils/template/MenuInitialTemplate.ts
@@ -2,19 +2,23 @@ export default class MenuInitialTemplate {
   public static EJS = '<% const { id, name, meta, items } = menu; -%>';
 
   public static JSON = `${MenuInitialTemplate.EJS}
-<% const mapChildren = (children) => { -%>
-  <%_ return children.map((child) => { -%>
-    <%_ const { id, label, order, meta, children } = child; -%>
-    <%_ return {
-      id,
-      label,
-      order,
-      meta,
-      children: children?.length ? mapChildren(children) : [],
-    }
-    %>
-  <%_ }); %>
-<% }; -%>
+<%
+  const mapChildren = (children) => {
+    return children.map((child) => {
+      const { id, label, order, meta, children, template } = child;
+      if (template) {
+        return template;
+      }
+      return {
+        id,
+        label,
+        order,
+        meta,
+        children: children?.length ? mapChildren(children) : [],
+      }
+    });
+  };
+%>
 {
   "id": <%= id %>,
   "name": "<%= name %>",
@@ -23,36 +27,42 @@ export default class MenuInitialTemplate {
 }`;
 
   public static XML = `${MenuInitialTemplate.EJS}
-<% const metaTags = (meta) => { -%>
-<% return meta.map(({name, required, type}) => { -%>
-<% return \`<meta name="\${name}" required="\${required}" type="\${type}" />\`; %>
-<% }); %>
-<% }; -%>
-<% const childrenMetaTags = (meta) => { -%>
-<% return Object.keys(meta).map((key) => { -%>
-<% return \`<meta key="\${key}" value="\${meta[key]}" />\`; %>
-<% }); %>
-<% }; -%>
-<% const mapChildren = (children, level) => { -%>
-<% return children.map((child) => { -%>
-<% const { id, label, order, meta, children } = child; -%>
-<% let spaces = '  '; -%>
-<% for (let i = level; i > 0; i--) { -%>
-<% spaces += '  '; -%>
-<% } -%>
-<% let item = \`  \${spaces}<item id="\${id}" label="\${label}" order="\${order}">\\n\` -%>
-<% if (meta) {
-  item += \`\${spaces}    \${childrenMetaTags(meta).join('\\n    ' + spaces)}\\n\`;
-} -%>
-<% if (children?.length) {
-  item += \`\${spaces}    <children>
-\${mapChildren(children, level + 2).join('\\n')}
-\${spaces}    </children>\\n\`;
-} -%>
-<% item += \`  \${spaces}</item>\`; -%>
-<% return item; %>
-<% }); %>
-<% }; -%>
+<% 
+  const metaTags = (meta) => {
+    return meta.map(({name, required, type}) => {
+      return \`<meta name="\${name}" required="\${required}" type="\${type}" />\`;
+    })
+  };
+  const childrenMetaTags = (meta) => {
+    return Object.keys(meta).map((key) => {
+      return \`<meta key="\${key}" value="\${meta[key]}" />\`
+    });
+  };
+  const mapChildren = (children, level) => {
+    return children.map((child) => {
+      const { id, label, order, meta, children, template } = child;
+      let spaces = '  ';
+      for (let i = level; i > 0; i--) {
+        spaces += '  ';
+      }
+      if (template) {
+        const formattedTemplate = template.split('\\n').map(chunk => spaces + '  ' + chunk).join('\\n')
+        return formattedTemplate;
+      }
+      let item = spaces + \`  <item id="\${id}" label="\${label}" order="\${order}">\\n\`
+      if (meta) {
+        item += spaces + '    ' + childrenMetaTags(meta).join('\\n    ' + spaces) + '\\n';
+      }
+      if (children?.length) {
+        item += spaces + '    <children>\\n';
+        item += mapChildren(children, level + 2).join('\\n') + '\\n';
+        item += spaces + '    </children>\\n';
+      }
+      item += spaces + '  </item>';
+      return item
+    });
+  };
+-%>
 <menu id="<%= id %>" name="<%= name %>">
 <%- meta?.length ? '  ' + metaTags(meta).join('\\n  ') + '\\n' : '' -%>
 <% if(items?.length) { -%>
@@ -63,8 +73,25 @@ export default class MenuInitialTemplate {
 </menu>`;
 
   public static PLAIN = `${MenuInitialTemplate.EJS}
+<%
+  const mapChildren = (children) => {
+    return children.map(child => {
+      const { id, label, order, meta, children, template } = child;
+      if (template) {
+        return template;
+      }
+      return JSON.stringify({
+        id,
+        label,
+        order,
+        meta,
+        children: children?.length ? mapChildren(children) : undefined,
+      }, null, 2)
+    });
+  };
+%>
 id = <%= id %>;
 name = "<%= name %>";
 meta = <%- JSON.stringify(meta, null, 2) %>;
-items = <%- JSON.stringify(items, null, 2) %>;`;
+items = <%- items?.length ? mapChildren(items).join(',\\n') : [] %>;`;
 }
