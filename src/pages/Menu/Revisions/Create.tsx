15bfd4ec2de6c127532afb90b21fa115f9d3a71d
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-02-28 23:24:55 -0300

    feat: Menu Revisions Create renderMeta and renderMetaChanges

--- src/pages/Menu/Revisions/Create.tsx
+++ src/pages/Menu/Revisions/Create.tsx
@@ -37,11 +37,39 @@ const CreateRevision = () => {
     items = items && removeTypename(items);
     meta = meta && removeTypename(meta);
     if (!currentRevision) return { name, meta, items, template, templateFormat };
+    const updatedMeta = currentRevision.snapshot.meta?.map((item: any, index: number) => {
+      const current = meta?.[index];
+      if (!current || item.id !== current.id || item.type !== current.type) return null;
+      return current;
+    });
+    meta = [...(updatedMeta || []), ...(meta || [])].filter((item, index, arr) => {
+      if (item === null) return true;
+      return arr.findIndex(i => i?.id === item.id) === index;
+    });
+    console.log(
+      deepDiff(currentRevision.snapshot, { name, meta, items, template, templateFormat }),
+    );
     const diff = Object.entries(
       deepDiff(currentRevision.snapshot, { name, meta, items, template, templateFormat }),
     )
       .filter(([key, value]) => {
-        if (value.to === undefined) return false;
+        if (value.to === undefined) {
+          if (key.includes('meta')) {
+            const index = key.split('.')[1];
+            if (!meta[index]) {
+              value.to = null;
+              return true;
+            }
+          }
+          if (key.includes('items')) {
+            const index = key.split('.')[1];
+            if (!items[index]) {
+              value.to = null;
+              return true;
+            }
+          }
+          return false;
+        }
         return true;
       })
       .reduce((acc, [key, value]) => {
@@ -68,7 +96,8 @@ const CreateRevision = () => {
     return (
       <Box className="flex">
         <Typography variant="body1" component="span">
-          {t('common.from')}: <b>{from}</b>
+          {t('common.from')}:{' '}
+          {from ? <b>{from}</b> : <i className="text-gray-500">{t('common.none')}</i>}
         </Typography>
         <ArrowForwardIcon sx={{ mx: '0.5rem' }} />
         <Typography variant="body1" component="span">
@@ -76,6 +105,90 @@ const CreateRevision = () => {
         </Typography>
       </Box>
     );
+  };
+
+  const renderMetaChanges = (from: any, to: any) => {
+    const fields = Object.keys(to);
+    return fields.map(field => {
+      if (field === 'id' || field === 'order') return null;
+      const fieldName =
+        field === 'type'
+          ? t(`menu.fields.meta.${field}.title`, { count: 1 })
+          : t(`menu.fields.meta.${field}`);
+      let fieldValueFrom =
+        field === 'type' ? t(`menu.fields.meta.${field}.${from?.[field]}`) : from?.[field];
+      let fieldValueTo = field === 'type' ? t(`menu.fields.meta.${field}.${to[field]}`) : to[field];
+      if (field === 'required' || field === 'enabled') {
+        fieldValueFrom = fieldValueFrom ? t('common.yes') : t('common.no');
+        fieldValueTo = fieldValueTo ? t('common.yes') : t('common.no');
+      }
+      if (!fieldValueFrom && !fieldValueTo) return null;
+      if (from?.[field] === undefined || from?.[field] === null) {
+        if (field === 'name') return null;
+        return (
+          <Typography
+            variant="body1"
+            component="span"
+            key={field}
+            sx={{ ml: '1rem', my: '0.25rem' }}
+          >
+            <b>{fieldName}</b>: {fieldValueTo}
+          </Typography>
+        );
+      }
+      return (
+        <Box className="flex flex-col my-1 ml-4" key={field}>
+          <Typography variant="body1" component="span">
+            <b>{fieldName}</b>:
+          </Typography>
+          {renderChanges(fieldValueFrom, fieldValueTo)}
+        </Box>
+      );
+    });
+  };
+
+  const renderMeta = () => {
+    if (!menuDiff?.meta)
+      return (
+        <Typography variant="body1" component="p">
+          {t('common.noChanges')}
+        </Typography>
+      );
+    return Object.keys(menuDiff.meta).map(index => {
+      const from = data.menu.currentRevision.snapshot.meta[index];
+      const to = menuDiff.meta[index];
+      if (to === null)
+        return (
+          <Box className="flex flex-col my-2" key={index}>
+            <Typography variant="h5" component="h5" className="line-through">
+              {from.order}. {from.name}
+            </Typography>
+            <Typography variant="body1" component="p" sx={{ ml: '1rem', color: 'error.main' }}>
+              {t('common.deleted', { context: 'male' })}
+            </Typography>
+          </Box>
+        );
+      if (!from || (to.id && from.id !== to.id) || (to.type && from.type !== to.type))
+        return (
+          <Box className="flex flex-col my-2" key={index}>
+            <Typography variant="h5" component="h5">
+              {to.order}. {to.name}
+            </Typography>
+            <Typography variant="body1" component="p" sx={{ ml: '1rem', color: 'success.main' }}>
+              {t('common.added', { context: 'male' })}
+            </Typography>
+            {renderMetaChanges(from, to)}
+          </Box>
+        );
+      return (
+        <Box className="flex flex-col my-2" key={index}>
+          <Typography variant="h5" component="h5">
+            {to.order || from.order}. {to.name || from.name}:
+          </Typography>
+          {renderMetaChanges(from, to)}
+        </Box>
+      );
+    });
   };
 
   if (error)
@@ -125,6 +238,12 @@ const CreateRevision = () => {
           {t('menu.fields.name')}:
         </Typography>
         {renderChanges(data?.menu.currentRevision?.snapshot.name, menuDiff?.name)}
+      </Box>
+      <Box className="flex flex-col my-4">
+        <Typography variant="h4" component="h4">
+          {t('menu.fields.meta.title', { count: 2 })}:
+        </Typography>
+        {renderMeta()}
       </Box>
     </Box>
   );
