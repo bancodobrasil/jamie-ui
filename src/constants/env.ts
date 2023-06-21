759edf17841b91c39b9f5e07538bceaca773cabe
Elias Feijó de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2023-06-21 13:13:51 -0300

    feat: jamie conditions feature env config

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -9,3 +9,6 @@ const getConfig = varname => {
 export const JAMIE_UI_BASE_URL = getConfig('${JAMIE_UI_BASE_URL}');
 export const JAMIE_KEYCLOAK_BASE_URL = getConfig('${JAMIE_KEYCLOAK_BASE_URL}');
 export const JAMIE_API_BASE_URL = getConfig('${JAMIE_API_BASE_URL}');
+
+// The conditions feature requires FeatWS to be installed and configured.
+export const JAMIE_FEATURE_CONDITIONS = getConfig('${JAMIE_FEATURE_CONDITIONS}');
