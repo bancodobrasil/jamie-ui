180c4471dd97adb242b28b00ee362197d36917c7
Raphael Pinto <raphaelcpinto@gmail.com>
2023-09-05 08:33:08 -0300

    fix: attrib create

--- src/pages/Menu/Edit/Edit.tsx
+++ src/pages/Menu/Edit/Edit.tsx
@@ -66,8 +66,8 @@ export const EditMenu = () => {
     setHasConditions(data.menu.hasConditions);
     setParameters(data.menu.parameters);
     setMetaWithErrors(
-      data?.menu.meta
-        ?.map(m => {
+      (data?.menu.meta || [])
+        .map(m => {
           const { __typename, ...rest } = m;
           if (!rest.defaultValue) rest.defaultValue = '';
           return { ...rest, errors: {} };
