73ae9b0fba422d707b758e5d31911bfe5c052389
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-09-21 19:18:45 -0300

    feat: add validation in number type

--- src/components/Menu/Forms/Attributes/FormAttributes.tsx
+++ src/components/Menu/Forms/Attributes/FormAttributes.tsx
@@ -196,7 +196,11 @@ export function FormAttributes({ meta, setMeta, loadingSubmit, onSubmit, onBack,
                 updatedMeta[index].action = EnumInputAction.UPDATE;
               }
               updatedMeta[index].defaultValue = value;
-              updatedMeta[index].errors.defaultValue = '';
+              if (value.trim() === '' && m.required === true) {
+                updatedMeta[index].errors.defaultValue = t('form.validation.must_have_defaulValue');
+              } else {
+                updatedMeta[index].errors.defaultValue = '';
+              }
               setMeta(updatedMeta);
             }}
             InputLabelProps={{
